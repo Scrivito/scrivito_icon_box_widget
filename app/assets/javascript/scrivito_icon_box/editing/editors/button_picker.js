dc28a43b107d04b002564067afea0e182ea2a3cf
Gert Geidel <gert.geidel@infopark.de>
2014-08-05 16:26:04 +0200

    Refractor Java Script

--- app/assets/javascript/scrivito_icon_box/editing/editors/button_picker.js
+++ app/assets/javascript/scrivito_icon_box/editing/editors/button_picker.js
@@ -1,9 +1,17 @@
-(function($) {
+(function($, app) {
   'use strict';
 
   $(function() {
-    scrivito.on('load', function() {
-      return $('body').on('click', '.button-picker', function(event) {
+
+    app.buttonPickerEditor = {
+      // set selector for Editor
+      selector: '.button-picker',
+      
+      // Function that will be called on scrvito load
+      initFunction: function(index, elem) {},
+      
+      // set function triggert on click
+      clickFunction: function(event) {
         var cmsField = $(this);
         var text = cmsField.data('picker-content');
 
@@ -11,8 +19,15 @@
         cmsField.siblings().removeClass('selected');
 
         return cmsField.scrivito('save', text);
+      },
+    };
+
+    // Set click event
+    scrivito.on('load', function() {
+      return $('body').on('click', buttonPickerEditor.selector, function(event) {
+        buttonPickerEditor.clickFunction.bind(event)
       });
     });
   });
 
-})(jQuery);
+})(jQuery, this);
